2ba73a2f66395ac1f91ce2ca215699e9c5c6f609
Janis Svilans <janis.svilans@mapon.com>
2021-04-16 17:40:48 +0300

    added function_exists

--- classes/src/Operations/divideinto.php
+++ classes/src/Operations/divideinto.php
@@ -19,27 +19,30 @@ use Matrix\Operators\Division;
  * @return Matrix
  * @throws Exception
  */
-function divideinto(...$matrixValues)
-{
-    if (count($matrixValues) < 2) {
-        throw new Exception('Division operation requires at least 2 arguments');
-    }
+if (!function_exists('Matrix\divideinto')) {
+  function divideinto(...$matrixValues): Matrix
+  {
+      if (count($matrixValues) < 2) {
+          throw new Exception('Division operation requires at least 2 arguments');
+      }
 
-    $matrix = array_pop($matrixValues);
-    $matrixValues = array_reverse($matrixValues);
+      $matrix = array_pop($matrixValues);
+      $matrixValues = array_reverse($matrixValues);
 
-    if (is_array($matrix)) {
-        $matrix = new Matrix($matrix);
-    }
-    if (!$matrix instanceof Matrix) {
-        throw new Exception('Division arguments must be Matrix or array');
-    }
+      if (is_array($matrix)) {
+          $matrix = new Matrix($matrix);
+      }
+      if (!$matrix instanceof Matrix) {
+          throw new Exception('Division arguments must be Matrix or array');
+      }
 
-    $result = new Division($matrix);
+      $result = new Division($matrix);
 
-    foreach ($matrixValues as $matrix) {
-        $result->execute($matrix);
-    }
+      foreach ($matrixValues as $matrix) {
+          $result->execute($matrix);
+      }
 
-    return $result->result();
+      return $result->result();
+  }
+
 }
